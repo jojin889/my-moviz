8a27b166e79a39ee6deb261943939a178af0d27d
jonathan theron <jonathan.theron@gmx.fr>
2020-09-02 11:31:32 +0200

    first commit

--- app.js
+++ app.js
@@ -59,9 +59,8 @@ app.get('*', (req, res) => {
 
 
 //
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port);
 
-console.log(`listening on ${port}`);
 
 module.exports = app;
